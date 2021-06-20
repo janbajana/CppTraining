1581be3b600e3a6d57c163aae03a9261f666b129
Jan Bajana <jbajana@snapchat.com>
2021-06-20 22:11:32 +0200

    Lesson 1

--- apps/LessonOne/TemplateClass.hpp
+++ apps/LessonOne/TemplateClass.hpp
@@ -1,15 +1,57 @@
 #pragma once
 
+#include <atomic>
 #include <memory>
+#include <cmath>
+#include <stdio.h>
+#include <mutex>
 
 template <typename Scalar>
 class TemplateClass
 {
 public:
     TemplateClass() = default;
+    TemplateClass(Scalar _x, Scalar _y, Scalar _z) : x(_x), y(_y), z(_z) {}
+
+    Scalar length() const
+    {
+        // std::lock_guard<std::mutex> l(mutex);
+        ++timesCalled;
+        std::cout << "TemplateClass::length() const " << timesCalled << " - ";
+        return std::sqrt(x * x + y * y + z * z);
+    }
+
+    Scalar length()
+    {
+        // std::lock_guard<std::mutex> l(mutex);
+        ++timesCalled;
+        std::cout << "TemplateClass::length() " << timesCalled << " - ";
+        return std::sqrt(x * x + y * y + z * z);
+    }
+
+    // What compiler does here? Compiler creates name mangled free function.
+    // float __ZNK3TemplateClass8lengthEv(TemplateClass const * const this)
+    // {
+    //     return std::sqrt(this->x * this->x + this->y * this->y + this->z * this->z);
+    // }
+
+    Scalar getCheckSum() const
+    {
+        return timesCalled;
+    }
 
 private:
     Scalar x;
     Scalar y;
     Scalar z;
+
+    // This code is toxics. This may cause race condition.
+    // Because we are writing in "length() const". Const should mean data race free.
+    mutable std::atomic<int> timesCalled{ 0 };
+
+    // so we have to add mutex
+    // but std::atomic is faster.
+    // mutable std::mutex mutex;
+
+    // mutexts and atomics are not copyable. Be aware of it.
 };
--- apps/LessonOne/main.cpp
+++ apps/LessonOne/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <utility>
 
 #include "Macros.hpp"
 #include "ClassOne.hpp"
@@ -9,42 +13,67 @@
 using namespace std;
 
 // TODO integrate google test instead.
+// https://github.com/kigster/cmake-project-template/blob/master/.gitmodules
 
 // CppCon 2015: Richard Powell “The Importance of Being const"
 // https://www.youtube.com/watch?v=Y1KOuFYtTF4
 
-void validExpression();
+static const char* const HEADER = "\nLessonOne © 2021 Monkey Claps Inc.\n\n";
+static const char* const USAGE = "Usage:\n\tdivider <numerator> <denominator>\nDescription:\n\tComputes the result of a fractional division.\n";
 
-static const char *const HEADER = "\nLessonOne © 2021 Monkey Claps Inc.\n\n";
-static const char *const USAGE = "Usage:\n\tdivider <numerator> <denominator>\nDescription:\n\tComputes the result of a fractional division.\n";
-
-static const char *const TESTS[]{
+static const char* const TESTS[]{
     "validExpression\0",
-    "Test2\0"};
+    "Test2\0"
+};
 
-void validExpression()
+/**
+ * Test cons expressions.
+ */
+void
+validExpression()
 {
-    int a[] = {1, 2, 3};
+    int a[] = { 1, 2, 3 };
 
-    int *b = a;
+    int* b = a;
     b++;
     *b = 20;
 
-    int const *c = a;
+    int const* c = a;
     c++;
     // *c = 20;             //invalid expression
 
-    int *const d = a;
+    int* const d = a;
     // d++;                 //invalid expression
     *d = 20;
 
-    int const *const e = a;
+    int const* const e = a;
     // e++;                 //invalid expression
     // *e = 20;             //invalid expression
-    std::cout << *e << "\n";
+    std::cout << "e = " << *e << "\n";
+
+    int i = 3; // i is not declared const
+    const int& rci = i;
+    const_cast<int&>(rci) = 4; // OK: modifies i
+    std::cout << "i = " << i << '\n';
 }
 
-int main(int argc, const char *argv[])
+/**
+ * Test const expression in function.
+ */
+void
+constFunction()
+{
+    static TemplateClass<float> myVector{ 5, 5, 5 };
+    std::cout << "length = " << myVector.length() << '\n';
+
+    // timesCalled is not thread safety. But length() signalizes that function should only read.
+    // so mutex has to be added. But mutex is heavy so atomic variable would be better option.
+    static const TemplateClass<float> myVector2{ 6, 6, 6 };
+    std::cout << "length = " << myVector2.length() << '\n';
+}
+
+int
+main(int argc, const char* argv[])
 {
     std::cout << HEADER;
 
@@ -55,21 +84,48 @@ int main(int argc, const char *argv[])
         return 1;
     }
 
-    const char *test = argv[1];
-
-    TemplateClass<float> myVector;
+    const char* test = argv[1];
 
     // run a tes.
-    std::cout << "Running test: " << std::string(test) << std::endl;
+    if (strcmp(test, TESTS[0]) != 0)
+        std::cerr << "The test dos not exist: " << std::string(test) << std::endl;
+    else
+        std::cout << "Running test: " << std::string(test) << std::endl;
 
     if (strcmp(test, TESTS[0]) == 0)
     {
         validExpression();
+
+        std::thread refresh_thread = std::thread(
+            [&]() {
+                std::chrono::milliseconds time(10);
+                size_t loops = 4;
+                while (loops > 0)
+                {
+                    std::this_thread::sleep_for(time);
+                    std::cout << "refresh_thread" << std::endl;
+                    constFunction();
+                    --loops;
+                }
+            });
+
+        std::thread refresh_thread2 = std::thread(
+            [&]() {
+                std::chrono::milliseconds time(10);
+                size_t loops = 4;
+                while (loops > 0)
+                {
+                    std::this_thread::sleep_for(time);
+                    std::cout << "refresh_thread2" << std::endl;
+                    constFunction();
+                    --loops;
+                }
+            });
+
+        refresh_thread.join();
+        refresh_thread2.join();
     }
 
-    // use arv here
-    CT_UNUSED(myVector);
-
-    std::cout << "Program finished successfully!\n";
+    std::cout << "\nProgram finished successfully!\n";
     return 0;
 }
